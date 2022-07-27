1b0312c9b43f6cadc73ec4af57431e9fb4ea0b18
IsaacDavid <scpereyraestrada@gmail.oom>
2022-07-26 23:42:39 -0500

    push

---
+++ src/main/java/com/upchiapas/tripadvisor/controllers/AnimalController.java
@@ -0,0 +1,58 @@
+package com.upchiapas.tripadvisor.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+public class AnimalController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+    @FXML
+    private Button BtNBorrego;
+
+    @FXML
+    private Button BtNChiken;
+
+    @FXML
+    private Button BtNGanado;
+
+    @FXML
+    private Button BtNGuajolote;
+
+    @FXML
+    private Button BtNPorqui;
+
+    @FXML
+    void BtNBorregoOnMouseCliked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void BtNChikenOnMouseClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void BtNGanadoOnMouseCliced(MouseEvent event) {
+
+    }
+
+    @FXML
+    void BtNGuajoloteOnMouseClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void BtNPorquiOnMouseClicked(MouseEvent event) {
+
+    }
+    @FXML
+    void initialize() {
+    }
+
+}
